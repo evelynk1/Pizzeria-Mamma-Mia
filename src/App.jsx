a14463e24eea9d4621613eb260b2afbe4ca1bf26
evelynk1 <evelynk195@gmail.com>
2026-04-05 16:23:54 -0400

    Se agrega carpeta backend y se levanta el servidor en puerto 5000

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,11 @@
 import { useState } from 'react'
 import './App.css'
 import Navbar from './components/navbar/Navbar'
-import Home from './components/home/Home'
+// import Home from './components/home/Home'
 import Footer from './components/footer/Footer'
 import Cart from './components/cart/Cart'
-import Login from './components/auth/Login'
-import Register from './components/auth/Register'
+// import Login from './components/auth/Login'
+// import Register from './components/auth/Register'
 
 function App() {
   const [view, setView] = useState("home");
@@ -13,9 +13,9 @@ function App() {
   return (
     <>
       <Navbar setView={setView} />
-      {view === "home" && <Home />}
+      {/* {view === "home" && <Home />}
       {view === "register" && <Register />}
-      {view === "login" && <Login />}
+      {view === "login" && <Login />} */}
       <Cart />
       <Footer />
     </>
