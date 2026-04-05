4dbfa540f7ce5962acc0faa8e46c1fa5b3bf3038
evelynk1 <evelynk195@gmail.com>
2026-04-05 19:19:17 -0400

    se agrega componente Profile con email y botón de cerrar sesión

--- src/pages/Profile.jsx
+++ src/pages/Profile.jsx
@@ -1,11 +1,41 @@
+import { useNavigate } from "react-router-dom";
+
 const Profile = () => {
+  const navigate = useNavigate();
+
+  // Simulación de usuario 
+  const user = {
+    email: "usuario@email.com",
+  };
+
+  const handleLogout = () => {
+    //  limpiar token o estado global
+    alert("Sesión cerrada");
+
+    // Redirige al home
+    navigate("/");
+  };
+
   return (
-    <div className="container my-5 text-center">
-      <h2 className="mb-4">Perfil de Usuario</h2>
-      <p className="mb-3">
-        <strong>Email:</strong> usuario@correo.com
-      </p>
-      <button className="btn btn-dark">Cerrar sesión</button>
+    <div className="auth-container">
+      <div className="auth-box text-center">
+
+        <h2 className="mb-4">Perfil</h2>
+
+        <p className="mb-3">
+          <strong>Email:</strong>
+        </p>
+
+        <p className="mb-4 text-warning">{user.email}</p>
+
+        <button
+          className="btn btn-danger w-100"
+          onClick={handleLogout}
+        >
+          Cerrar sesión
+        </button>
+
+      </div>
     </div>
   );
 };
