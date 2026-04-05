a27f4979095eb924524a55cdbc03efe9781f51bb
evelynk1 <evelynk195@gmail.com>
2026-04-05 15:03:30 -0400

    se importa pizzas.js en Home y renderizar CardPizza dinámicamente

--- src/components/card/CardPizza.jsx
+++ src/components/card/CardPizza.jsx
@@ -25,4 +25,29 @@ const CardPizza = ({ name, price, ingredients, img }) => {
   );
 };
 
+// const CardPizza = ({ name, price, ingredients, img }) => {
+//   return (
+//     <div className="card mb-4 border-naranjo">
+//       <img src={img} className="card-img-top" alt={name} />
+//       <div className="card-body">
+//         <h5 className="card-title">Pizza {name}</h5>
+//         <p className="card-text">
+//           <strong>Ingredientes:</strong> {ingredients.join(", ")}
+//         </p>
+//         <p className="card-text">
+//           <strong>Precio:</strong> ${price.toLocaleString()}
+//         </p>
+//         <div className="d-flex justify-content-between">
+//           <button className="btn btn-outline-dark">
+//             <i className="bi bi-eye"></i> Ver más
+//           </button>
+//           <button className="btn btn-dark">
+//             <i className="bi bi-cart-plus"></i> Añadir
+//           </button>
+//         </div>
+//       </div>
+//     </div>
+//   );
+// };
+
 export default CardPizza;
