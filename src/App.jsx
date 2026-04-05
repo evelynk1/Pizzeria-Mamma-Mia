9e413f18f027f0c5ceb96b33963e1d9783e90b58
evelynk1 <evelynk195@gmail.com>
2026-04-05 14:35:14 -0400

    se  comenta Login y Register en App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -3,8 +3,8 @@ import './App.css'
 import Navbar from './components/navbar/Navbar'
 import Home from './components/home/Home'
 import Footer from './components/footer/Footer'
-import Login from './components/auth/Login'
-import Register from './components/auth/Register'
+// import Login from './components/auth/Login'
+// import Register from './components/auth/Register'
 
 function App() {
   const [view, setView] = useState("home");
@@ -13,8 +13,8 @@ function App() {
     <>
       <Navbar setView={setView} />
       {view === "home" && <Home />}
-      {view === "register" && <Register />}
-      {view === "login" && <Login />}
+      {/* {view === "register" && <Register />}
+      {view === "login" && <Login />} */}
       <Footer />
     </>
   );
